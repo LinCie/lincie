1bc9cacb322bad47f97180ed58ce96ee98a863cc
LinCie <aldiofernanda@protonmail.com>
2024-12-20 10:46:44 +0700

    feat: add prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
+  overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
+};
