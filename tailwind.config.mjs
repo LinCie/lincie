00549d7795a467ad17e2841cb344d93e5bc88323
LinCie <aldiofernanda@protonmail.com>
2024-12-26 22:57:03 +0700

    style: adjust coloring

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -23,8 +23,34 @@ export default {
                 },
             },
             fontFamily: {
-                'winter': ['Mountains of Christmas', 'system-ui'],
-            }
+                winter: ['Mountains of Christmas', 'system-ui'],
+            },
+            colors: {
+                polar: {
+                    0: '#2e3440',
+                    1: '#3b4252',
+                    2: '#434c5e',
+                    3: '#4c566a',
+                },
+                snow: {
+                    0: '#d8dee9',
+                    1: '#e5e9f0',
+                    2: '#eceff4',
+                },
+                frost: {
+                    0: '#8fbcbb',
+                    1: '#88c0d0',
+                    2: '#81a1c1',
+                    3: '#5e81ac',
+                },
+                aurora: {
+                    0: '#bf616a',
+                    1: '#d08770',
+                    2: '#ebcb8b',
+                    3: '#a3be8c',
+                    4: '#b48ead',
+                },
+            },
         },
     },
 }
