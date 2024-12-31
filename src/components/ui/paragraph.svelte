8b01617365f2af57306293ee02961bfb7df2a74b
LinCie <aldiofernanda@protonmail.com>
2025-01-01 01:56:47 +0700

    feat: add about page

---
+++ src/components/ui/paragraph.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { cn } from '@/lib/utils'
+
+    const { className, children, ...props } = $props()
+</script>
+
+<p class={cn('leading-7 [&:not(:first-child)]:mt-6', className)} {...props}>
+    {#if children}
+        {@render children()}
+    {:else}
+        fallback content
+    {/if}
+</p>
